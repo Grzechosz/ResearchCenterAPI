f31b325fb813b1fc5e5d2848b36502d18f5c9cd9
Grzechosz <grzechosz125@gmail.com>
2023-03-30 23:26:22 +0200

    added last crud operation to test commission and test  results

--- src/main/java/com/denert/app/rest/Controller/TestCommissionController.java
+++ src/main/java/com/denert/app/rest/Controller/TestCommissionController.java
@@ -1,14 +1,12 @@
 package com.denert.app.rest.Controller;
 
+import com.denert.app.rest.Models.Agreement;
 import com.denert.app.rest.Models.TestCommission;
 import com.denert.app.rest.Repo.TestCommissionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,10 +21,36 @@ public class TestCommissionController {
         return testCommissionRepo.findAll();
     }
 
+    @GetMapping(value = "/test-commissions/{id}")
+    public TestCommission getCommission(@PathVariable long id) {
+        return testCommissionRepo.findById(id).get();
+    }
+
     @Transactional
     @PostMapping(value = "/test-commissions")
     public ResponseEntity<TestCommission> saveTestCommission(@RequestBody TestCommission testCommission) {
         testCommissionRepo.save(testCommission);
         return ResponseEntity.ok(testCommission);
+    }
+
+    @PutMapping(value = "/test-commissions/{id}")
+    public ResponseEntity<TestCommission> updateTestCommission(@PathVariable long id, @RequestBody TestCommission testCommission) {
+        TestCommission updateCommission = testCommissionRepo.findById(id).get();
+
+        updateCommission.setDescription(testCommission.getDescription());
+        updateCommission.setDate(testCommission.getDate());
+        updateCommission.setTestResult(testCommission.getTestResult());
+        updateCommission.setProject(testCommission.getProject());
+        updateCommission.setUser(testCommission.getUser());
+
+        testCommissionRepo.save(updateCommission);
+        return ResponseEntity.ok(updateCommission);
+    }
+
+    @DeleteMapping(value = "/test-commissions/{id}")
+    public String deleteCommission(@PathVariable long id) {
+        TestCommission deleteCommission = testCommissionRepo.findById(id).get();
+        testCommissionRepo.delete(deleteCommission);
+        return "Deleted test commission with id: "+id;
     }
 }
--- src/main/java/com/denert/app/rest/Controller/TestResultsController.java
+++ src/main/java/com/denert/app/rest/Controller/TestResultsController.java
@@ -1,6 +1,7 @@
 package com.denert.app.rest.Controller;
 
 import com.denert.app.rest.Models.Address;
+import com.denert.app.rest.Models.Agreement;
 import com.denert.app.rest.Models.Project;
 import com.denert.app.rest.Models.TestResults;
 import com.denert.app.rest.Repo.ProjectRepo;
@@ -22,6 +23,11 @@ public class TestResultsController {
         return resultRepo.findAll();
     }
 
+    @GetMapping(value = "/results/{id}")
+    public TestResults getResults(@PathVariable long id) {
+        return resultRepo.findById(id).get();
+    }
+
     @PostMapping(value = "/results")
     public ResponseEntity<TestResults> saveResults(@RequestBody TestResults testResults) {
         resultRepo.save(testResults);
@@ -35,5 +41,12 @@ public class TestResultsController {
         updatedResult.setTestCommission(testResults.getTestCommission());
         resultRepo.save(updatedResult);
         return ResponseEntity.ok(updatedResult);
+    }
+
+    @DeleteMapping(value = "/results/{id}")
+    public String deleteResults(@PathVariable long id) {
+        TestResults deleteResults = resultRepo.findById(id).get();
+        resultRepo.delete(deleteResults);
+        return "Deleted test result with id: "+id;
     }
 }
